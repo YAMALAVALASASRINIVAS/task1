ee4d653685207fc94cba6a291ececb0c805c89f1
YAMALAVALASASRINIVAS <srinusrinivas2646@gmail.com>
2024-03-19 23:40:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.style.backgroundColor = '#fff'; // Change background color on scroll
+    } else {
+      navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+    }
+  });
